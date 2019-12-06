e391b1540760beb81d263d30fe5feb6d3da17c8c
felix <salim.f.aa@m.titech.ac.jp>
2019-12-06 15:00:17 +0900

    [Update] Added bidirectional reaction

--- R/rhea_table_curation.R
+++ R/rhea_table_curation.R
@@ -1,4 +1,4 @@
-setwd("~/digzyme/")
+setwd("~/digzyme/rhea_rmd")
 
 # Function to curate direction by directing compound, e.g. O2
 curate_direction <- function(dframe, id) {
@@ -7,7 +7,7 @@ curate_direction <- function(dframe, id) {
     dplyr::mutate(direction = "L>R")
   product <- dframe %>%
     dplyr::slice(grep(id, R)) %>%
-    dplyr::mutate(direction = "L<R")
+    dplyr::mutate(direction = "L>R")
   directed <- dplyr::bind_rows(substrate, product)
 }
 
@@ -15,7 +15,7 @@ curate_direction <- function(dframe, id) {
 library(tidyverse)
 
 
-dframe <- read_tsv("rhea_id_side_compound.tsv")
+dframe <- read_tsv("data/rhea_id_side_compound.tsv")
 
 
 # Calculate compound usage frequency
@@ -24,7 +24,7 @@ compound_freq <- dframe %>%
   arrange(desc(n))
 
 
-rhea_table <- read_tsv("rhea_rheaid_participants.tsv")
+rhea_table <- read_tsv("data/rhea_rheaid_participants.tsv")
 
 
 # Make vectors of CHENI ID to curate with
@@ -45,6 +45,12 @@ directed_rhea <- reduce(.x = curated_dfs,
                         .f = dplyr::bind_rows) %>%
   dplyr::distinct(rheaid, .keep_all = TRUE) %>%
   dplyr::arrange(rheaid)
+
+
+# Bidirectional reactions table
+bidirectional_rhea <- rhea_table %>%
+  filter(!rheaid %in% directed_rhea$rheaid) %>%
+  mutate(direction = 'L<>R')
 
 
 # Extract compounds with frequent usage
@@ -79,6 +85,17 @@ main_directed <- dframe %>%
                      values_from = c(compoundID, compoundName))
 
 
+# Extract bidirectional reactions without common compounds as participant
+main_bidirectional <- dframe %>%
+  dplyr::filter(rheaid %in% bidirectional_rhea$rheaid,
+                !compoundID %in% frequent_compound) %>%
+  dplyr::group_by(rheaid, reactionSide, reactionEquation) %>%
+  dplyr::summarise(compoundID = paste(compoundID, collapse = ','),
+                   compoundName = paste(compoundName, collapse = ',')) %>%
+  tidyr::pivot_wider(names_from = reactionSide,
+                     values_from = c(compoundID, compoundName))
+
+
 # Extract reactions with cofactors as main participants
 cofactor_directed <- dframe %>%
   dplyr::filter(rheaid %in% directed_rhea$rheaid,
@@ -90,13 +107,28 @@ cofactor_directed <- dframe %>%
                      values_from = c(compoundID, compoundName))
 
 
+# Extract bidirectional reactions with cofactors as main participants
+cofactor_bidirectional <- dframe %>%
+  dplyr::filter(rheaid %in% bidirectional_rhea$rheaid,
+                compoundID %in% frequent_compound) %>%
+  dplyr::group_by(rheaid, reactionSide, reactionEquation) %>%
+  dplyr::summarise(compoundID = paste(compoundID, collapse = ','),
+                   compoundName = paste(compoundName, collapse = ',')) %>%
+  tidyr::pivot_wider(names_from = reactionSide,
+                     values_from = c(compoundID, compoundName))
+
+
 generic_main_directed <- cofactor_directed %>%
   dplyr::filter(!rheaid %in% main_directed$rheaid)
+generic_main_bidirectional <- cofactor_bidirectional %>%
+  dplyr::filter(!rheaid %in% main_bidirectional$rheaid)
 
 
 #
 cofactor_directed <- cofactor_directed %>%
   dplyr::filter(!rheaid %in% generic_main_directed$rheaid)
+cofactor_bidirectional <- cofactor_bidirectional %>%
+  dplyr::filter(!rheaid %in% generic_main_bidirectional$rheaid)
 
 
 # Final curated RHEA reactions
@@ -104,12 +136,26 @@ mono_reaction <- main_directed %>%
   tidyr::drop_na() %>%
   dplyr::slice(c(grep(',', compoundID_L, invert = TRUE)),
                grep(',', compoundID_R, invert = TRUE)) %>%
+  dplyr::inner_join(.,
+                    directed_rhea %>%
+                      dplyr::select(rheaid, direction),
+                    by = 'rheaid') %>%
   dplyr::distinct_all()
+bi_reaction <- main_bidirectional %>%
+  tidyr::drop_na() %>%
+  dplyr::slice(c(grep(',', compoundID_L, invert = TRUE)),
+               grep(',', compoundID_R, invert = TRUE)) %>%
+  dplyr::filter(compoundID_L != compoundID_R) %>%
+  dplyr::mutate(direction = 'L<>R') %>%
+  dplyr::distinct_all()
+
+full_reaction <- bind_rows(mono_reaction, bi_reaction)
 
 
 # Save curated dataframe to files
-write_tsv(main_directed, "rhea_directed_reactants.tsv")
-write_tsv(mono_reaction, "rhea_directed_mono_reactions.tsv")
-write_tsv(cofactor_directed, "rhea_directed_cofactors.tsv")
-write_tsv(generic_main_directed, "rhea_directed_generic.tsv")
-write_tsv(compound_freq, "rhea_compound_usage.tsv")
+write_tsv(main_directed, "data/rhea_directed_reactants.tsv")
+write_tsv(mono_reaction, "data/rhea_directed_mono_reactions.tsv")
+write_tsv(cofactor_directed, "data/rhea_directed_cofactors.tsv")
+write_tsv(generic_main_directed, "data/rhea_directed_generic.tsv")
+write_tsv(compound_freq, "data/rhea_compound_usage.tsv")
+write_tsv(full_reaction, 'data/rhea_direction_annotated_reactions.tsv')
